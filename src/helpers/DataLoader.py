ae54d17eb72db59eb8297ac8b392d4a199aa76dc
al186109 <al186109@alumnos.uacj.mx>
2023-07-06 22:39:29 -0600

    fix dataloader

--- src/helpers/DataLoader.py
+++ src/helpers/DataLoader.py
@@ -4,11 +4,8 @@ import pandas as pd
 class Dataloader:
     def __init__(self,path):
         self.path = path
-
-    def preprocessdata(self, data):
-        pass
         
-    def load_data(self.path):
+    def load_data(self):
         data = pd.read_csv(self.path)
         return data
         
